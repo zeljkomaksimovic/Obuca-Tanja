f19d4073081a0232ca143d7ac3e3254411170180
zeljkomaksimovic <zeljko.maksimovic@yandex.com>
2018-06-27 16:43:10 +0200

    finished home page

--- ObucaTanja/Controllers/HomeController.cs
+++ ObucaTanja/Controllers/HomeController.cs
@@ -3,16 +3,36 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using ObucaTanja.Models;
+
 
 namespace ObucaTanja.Controllers
 {
     public class HomeController : Controller
     {
+        private static readonly List<Img> images = new List<Img>
+        {
+            new Img{ Name = "modeli.jpg", Text="Man's infantri brow leather", Price=8990},
+            new Img{ Name = "modeli.jpg", Text="Man's infantri brow leather", Price=8990},
+            new Img{ Name = "modeli.jpg", Text="Man's infantri brow leather", Price=8990},
+            new Img{ Name = "modeli.jpg", Text="Man's infantri brow leather", Price=8990},
+            new Img{ Name = "modeli.jpg", Text="Man's infantri brow leather", Price=8990},
+            new Img{ Name = "modeli.jpg", Text="Man's infantri brow leather", Price=8990}
+        };
+
+        [HttpGet]
         public ActionResult Index()
+        {
+            return View(images);
+        }
+
+        [HttpGet]
+        public ActionResult Shop()
         {
             return View();
         }
 
+        [HttpGet]
         public ActionResult About()
         {
             ViewBag.Message = "Your application description page.";
@@ -20,6 +40,7 @@ namespace ObucaTanja.Controllers
             return View();
         }
 
+        [HttpGet]
         public ActionResult Contact()
         {
             ViewBag.Message = "Your contact page.";
